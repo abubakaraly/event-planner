b3099d082e970bdb39c32a8117f5594fa965a4b3
Abubakaraly <i222693@nu.edu.pk>
2025-03-12 21:20:48 +0500

    Initial commit: Set up Node.js event planner with GitHub Actions

--- src/testScript.js
+++
@@ -1,7 +0,0 @@
-const { filterEventsByCategory } = require("./events");
-
-test("Filters events by category", () => {
-  const birthdays = filterEventsByCategory("Birthdays");
-  expect(birthdays.length).toBe(1);
-  expect(birthdays[0].name).toBe("Birthday Party");
-});
